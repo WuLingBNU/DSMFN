b82a3858157977a4dea93d5d627eeadc505a4f12
WuLingBNU <154974412+WuLingBNU@users.noreply.github.com>
2025-06-28 19:25:35 +0800

    Update utils.py

--- utils/utils.py
+++ utils/utils.py
@@ -4,22 +4,11 @@ import numpy as np
 
 
 def set_seed(seed):
-    # 设置随机种子
     seed = 42
-
-    # 设置 Python 自身的随机种子
     random.seed(seed)
-
-    # 设置 NumPy 的随机种子
     np.random.seed(seed)
-
-    # 设置 PyTorch 的 CPU 随机种子
     torch.manual_seed(seed)
-
-    # 设置 PyTorch 的 CUDA 随机种子（如果使用 GPU）
     torch.cuda.manual_seed(seed)
-    torch.cuda.manual_seed_all(seed)  # 适用于多 GPU 设置
-
-    # 设置 PyTorch 后端随机种子
+    torch.cuda.manual_seed_all(seed)  
     torch.backends.cudnn.deterministic = True
     torch.backends.cudnn.benchmark = False
